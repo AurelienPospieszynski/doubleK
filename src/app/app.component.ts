cd580e3a0a5b350ef700800db075792ce32704c3
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2023-12-29 21:08:08 +0100

    Fixing console errors with angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { CarousselComponent } from './components/caroussel/caroussel.component';
@@ -9,9 +8,7 @@ import { SneakerGridComponent } from './components/big-section/sneaker-grid/snea
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
   imports: [
-    CommonModule,
     RouterOutlet,
     NavigationBarComponent,
     CarousselComponent,
