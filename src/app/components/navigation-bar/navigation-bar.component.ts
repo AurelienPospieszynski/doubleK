4a844d73aeadc78ae16ac3e0472394954a616efa
maxime <maxime.pospieszynski@gmail.com>
2024-01-16 23:11:35 +0100

    add sneaker grid part 2 (WIP) + search bar

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule],
+  imports: [MatButtonModule, MatMenuModule, FormsModule],
   templateUrl: './navigation-bar.component.html',
 })
-export class NavigationBarComponent {}
+export class NavigationBarComponent {
+  searchQuery: string = '';
+
+  search() {
+    console.log('Recherche en cours :', this.searchQuery);
+  }
+}
