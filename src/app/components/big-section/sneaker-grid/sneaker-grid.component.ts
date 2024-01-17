600462641d8287a43e6ad07e9513c9213a7a9928
maxime <maxime.pospieszynski@gmail.com>
2024-01-17 12:22:15 +0100

    add sneaker grid (WIP)

--- src/app/components/big-section/sneaker-grid/sneaker-grid.component.ts
+++ src/app/components/big-section/sneaker-grid/sneaker-grid.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { SNEAKERS } from '../../../../assets/database/sneakers';
 
 @Component({
   selector: 'app-sneaker-grid',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, CommonModule],
   templateUrl: './sneaker-grid.component.html',
 })
 export class SneakerGridComponent {
-
+  sneakers = SNEAKERS;
 }
