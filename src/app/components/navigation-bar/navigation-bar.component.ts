30ddfa5b822a52fd2a5a89216f073a5bf7e9e22e
maxime <maxime.pospieszynski@gmail.com>
2024-01-18 13:21:57 +0100

    add sneaker grid (WIP)

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, FormsModule],
+  imports: [MatButtonModule, MatMenuModule, FormsModule, MatIconModule],
   templateUrl: './navigation-bar.component.html',
 })
 export class NavigationBarComponent {
