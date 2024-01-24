e4a5cc0c714796a1ade224284f35a8eb9b76e20a
maxime <maxime.pospieszynski@gmail.com>
2024-01-25 00:01:49 +0100

    add accueil component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
-import { CarousselComponent } from './components/caroussel/caroussel.component';
-import { SneakerGridComponent } from './components/big-section/sneaker-grid/sneaker-grid.component';
+import { CarousselComponent } from './components/accueil/caroussel/caroussel.component';
+import { SneakerGridComponent } from './components/accueil/sneaker-grid/sneaker-grid.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { SneakerGridComponent } from './components/big-section/sneaker-grid/sneaker-grid.component';
+import { SneakerGridComponent } from './components/accueil/sneaker-grid/sneaker-grid.component';
 import { SneakerDetailComponent } from './components/big-section/sneaker-detail/sneaker-detail.component';
 
 export const routes: Routes = [
